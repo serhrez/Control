0708caf430406f60841b480c6ecaba15432fbba8
Sergey Reznichenko <mutexlock64@gmail.com>
2021-01-14 15:13:50 +0200

    make PremiumFeaturesVc

--- TodoApp/Screens_/PremiumFeatures_/PremiumFeaturesVc.swift
+++ TodoApp/Screens_/PremiumFeatures_/PremiumFeaturesVc.swift
@@ -8,22 +8,109 @@
 import Foundation
 import UIKit
 import Material
+import AttributedLib
 
 class PremiumFeaturesVc: UIViewController {
+
+    let containerView: UIScrollView = {
+        let view = UIScrollView()
+        view.contentLayoutGuide.widthAnchor.constraint(equalTo: view.frameLayoutGuide.widthAnchor).isActive = true
+        view.backgroundColor = UIColor(named: "TAAltBackground")
+        view.layer.cornerRadius = 16
+        view.scrollIndicatorInsets = .init(top: 16 / 2, left: 0, bottom: 16 / 2, right: 0)
+        return view
+    }()
+    let imageView: UIImageView = {
+        let view = UIImageView(image: UIImage(named: "premiumimage"))
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    let premiumLabel: UILabel = {
+        let view = UILabel()
+        view.minimumScaleFactor = 0.8
+        view.numberOfLines = 1
+        view.font = .systemFont(ofSize: 36, weight: .bold)
+        view.text = "Premium Features"
+        view.adjustsFontSizeToFitWidth = true
+        view.textColor = UIColor(named: "TAHeading")
+        return view
+    }()
+    let infoLabel: UILabel = {
+        let view = UILabel()
+        view.numberOfLines = 2
+        view.attributedText = "No additional purchases, just a ".at.attributed { attr in
+            attr.font(.systemFont(ofSize: 16, weight: .regular)).foreground(color: UIColor(named: "TAHeading")!)
+        } + "one-time purchase!".at.attributed { attr in
+            attr.font(.systemFont(ofSize: 16, weight: .bold)).foreground(color: UIColor(named: "TAHeading")!)
+        }
+        view.textAlignment = .center
+        return view
+    }()
+    let plusesStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 14
+        return stack
+    }()
+    let buyButton: NewCustomButton = {
+        let button = NewCustomButton()
+        button.addTarget(self, action: #selector(clickedOnBuy), for: .touchUpInside)
+        button.stateBackgroundColor = .init(highlighted: .red, normal: .hex("#FFE600"))
+        button.setTitle("Only $4,99", for: .normal)
+        button.setTitleColor(UIColor(named: "TAHeading")!, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .heavy)
+        button.layer.cornerRadius = 16
+        button.layer.cornerCurve = .continuous
+        return button
+    }()
+    let restoreButton: NewCustomButton = {
+        let button = NewCustomButton()
+        button.addTarget(self, action: #selector(clickedOnRestore), for: .touchUpInside)
+        button.setTitle("Restore Purchase", for: .normal)
+        button.opacityState = .init(highlighted: 0.5, normal: 1)
+        button.setTitleColor(.hex("#447bfe"), for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.titleLabel?.textAlignment = .center
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        let button = UIButton(type: .system)
-        button.setTitle("Only $4,99", for: .normal)
-        button.addTarget(self, action: #selector(clickedOnBuy), for: .touchUpInside)
-        button.backgroundColor = .yellow
-        view.layout(button).center().width(200).height(50)
-        let button2 = UIButton(type: .system)
-        button2.setTitle("Restore", for: .normal)
-        button2.addTarget(self, action: #selector(clickedOnRestore), for: .touchUpInside)
-        button2.backgroundColor = .green
-        view.layout(button2).centerX().centerY(100).width(200).height(50)
-        print("premium enabled: \(UserDefaultsWrapper.shared.isPremium)")
+        setupViews()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        containerView.flashScrollIndicators()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = UIColor(named: "TABackground")!
+        applySharedNavigationBarAppearance()
+        view.layout(containerView).leading(13).trailing(13).topSafe()
+        containerView.layout(imageView).top(22).leading(21) { _, _ in .greaterThanOrEqual }
+            .trailing(21) { _, _ in .lessThanOrEqual }.centerX().height(imageView.anchor.width).multiply(0.8216)
+        containerView.layout(premiumLabel).top(imageView.anchor.bottom, 33.2).leading(30).trailing(30)
+        containerView.layout(plusesStack).top(premiumLabel.anchor.bottom, 30).leading(30).trailing(30)
+        plusesStack.addArrangedSubview(getHorizontalStack("Unlimited Tasks"))
+        plusesStack.addArrangedSubview(getHorizontalStack("Easy to use reminders"))
+        plusesStack.addArrangedSubview(getHorizontalStack("Convenient use of tags"))
+        plusesStack.addArrangedSubview(getHorizontalStack("Constant improvements and new features"))
+        containerView.layout(buyButton).top(plusesStack.anchor.bottom, 35).leading(30).trailing(30).height(60)
+        containerView.layout(infoLabel).top(buyButton.anchor.bottom, 25).centerX().width(250).priority(999).leading() { _, _ in .greaterThanOrEqual }.trailing() { _, _ in .lessThanOrEqual }.bottom(25)
+        view.layout(restoreButton).top(containerView.anchor.bottom, 17).width(250).centerX().bottom(Constants.vcMinBottomPadding) { _, _ in .greaterThanOrEqual }
+    }
+    
+    func getHorizontalStack(_ text: String) -> UIStackView {
+        let check = CheckboxView()
+        check.tint = .hex("#447BFE")
+        check.configure(isChecked: true)
+        let textLabel = UILabel()
+        textLabel.font = .systemFont(ofSize: 16, weight: .regular)
+        textLabel.text = text
+        let horizontalStack = UIStackView(arrangedSubviews: [check, textLabel, UIView()])
+        horizontalStack.axis = .horizontal
+        horizontalStack.spacing = 9
+        return horizontalStack
     }
     
     @objc func clickedOnBuy() {
