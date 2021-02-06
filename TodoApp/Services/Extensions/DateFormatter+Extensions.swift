e074edee62d7ae9138d31bc108d9833a2aa2c89e
Sergey Reznichenko <mutexlock64@gmail.com>
2021-02-06 20:47:48 +0200

    date formatting

--- TodoApp/Services/Extensions/DateFormatter+Extensions.swift
+++ TodoApp/Services/Extensions/DateFormatter+Extensions.swift
@@ -6,11 +6,25 @@
 //
 
 import Foundation
-
+import SwiftDate
 extension DateFormatter {
+    fileprivate static let formatter = DateFormatter()
     static func str(from date: Date) -> String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MM/dd HH:mm"
-        return formatter.string(from: date)
+        if date.isToday {
+            formatter.dateFormat = "HH:mm"
+            return "Today " + formatter.string(from: date)
+        } else if date.isTomorrow {
+            formatter.dateFormat = "HH:mm"
+            return "Tomorrow " + formatter.string(from: date)
+        } else if date.isInside(date: Date(), granularity: .weekOfYear) {
+            formatter.dateFormat = "EEEE, HH:mm"
+            return formatter.string(from: date)
+        } else if date.isInside(date: Date().dateAt(.nextWeek), granularity: .weekOfYear) {
+            formatter.dateFormat = "E, d MMM HH:mm"
+            return formatter.string(from: date)
+        } else {
+            formatter.dateFormat = "d MMM yyyy, HH:mm"
+            return formatter.string(from: date)
+        }
     }
 }
