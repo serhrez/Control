b0f73d4313970d99876fa6709828d16f31e05c43
Sergey Reznichenko <mutexlock64@gmail.com>
2021-02-25 21:03:26 +0200

    change title

--- TodoApp/Screens_/AllTasksScreen_/AllProjectsVc.swift
+++ TodoApp/Screens_/AllTasksScreen_/AllProjectsVc.swift
@@ -136,7 +136,7 @@ class AllProjectsVc: UIViewController {
     
     private func setupNavigationBar() {
         applySharedNavigationBarAppearance(addBackButton: false)
-        title = "Today".localizable() + ", \(Date().toFormat("d MMMM"))"
+        title = Date().toFormat("EEEE") + ", \(Date().toFormat("d MMMM"))"
         
         navigationItem.leftBarButtonItem = menuButton
         navigationItem.rightBarButtonItems = [actionsButton, searchButton]
