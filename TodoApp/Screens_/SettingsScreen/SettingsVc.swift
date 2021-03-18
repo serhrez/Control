3add6962e48a078f6027328effde47db161b602b
Sergey Reznichenko <mutexlock64@gmail.com>
2021-03-18 15:59:27 +0200

    remove debug settings

--- TodoApp/Screens_/SettingsScreen/SettingsVc.swift
+++ TodoApp/Screens_/SettingsScreen/SettingsVc.swift
@@ -34,10 +34,6 @@ class SettingsVc: UIViewController {
                     return
                 }
                 self.router.openArchive()
-            }),
-            .init(text: "Debug Settings", imageName: "", imageWidth: 20, onClick: { [weak self] in
-                guard let self = self else { return }
-                self.navigationController?.pushViewController(DebugSettingsVc(), animated: true)
             })
         ])
     lazy var secondaryCollectionView = SettingsVcCollectionView(
