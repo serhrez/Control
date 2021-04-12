251d999b8d488c5219d0eb016857d11ae4c6a7db
Sergey Reznichenko <mutexlock64@gmail.com>
2021-04-12 18:53:36 +0300

    update fonts

--- TodoApp/Services/Fonts.swift
+++ TodoApp/Services/Fonts.swift
@@ -9,12 +9,12 @@ import Foundation
 import UIKit
 
 enum Fonts {
-    static let heading1 = UIFont.systemFont(ofSize: 28, weight: .bold)
-    static let heading2 = UIFont.systemFont(ofSize: 20, weight: .medium)
+    static let heading1 = UIFont.systemFont(ofSize: 28, weight: .semibold)
+    static let heading2 = UIFont.systemFont(ofSize: 20, weight: .regular)
     static let heading3 = UIFont.systemFont(ofSize: 18, weight: .regular)
-    static let heading4 = UIFont.systemFont(ofSize: 16, weight: .medium)
+    static let heading4 = UIFont.systemFont(ofSize: 16, weight: .regular)
     static let heading5 = UIFont.systemFont(ofSize: 15, weight: .regular)
-    static let heading6 = UIFont.systemFont(ofSize: 12, weight: .semibold)
+    static let heading6 = UIFont.systemFont(ofSize: 12, weight: .medium)
     
     static let custHeading1 = UIFont.systemFont(ofSize: 18, weight: .semibold)
     static let custHeading2 = UIFont.systemFont(ofSize: 36, weight: .bold)
